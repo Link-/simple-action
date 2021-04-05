bddda6732f9ee788450eb29bb59f02c4ba5ee000
Bassem Dghaidi <link-@github.com>
2021-04-05 20:35:27 +0200

    Add full implementation and workflow

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,26 @@
 const meta = require('../package.json');
-const { Command } = require('commander');
 const Invoker = require('./invoker');
+const core = require('@actions/core')
+const { Command } = require('commander');
 
 const program = new Command();
 
+/**
+ * We make use of the default option to fetch the input from our action
+ * with core.getInput() only when a value has not been supplied via the CLI.
+ * What this means is that, if you provide these parameters the values from
+ * the action will be ignored.
+ * 
+ * This will guarantee that this tool will operate as an action but has an
+ * alternative trigger via the CLI.
+ */
 program
   .version(meta.version)
-  .option('-d, --debug', 'Output debugging information')
-  .requiredOption('-c, --command <command name>', 'Command to execute')
-  .option('-t, --token <token>', 'Personal Access Token or GITHUB_TOKEN')
-  .option('-i, --issue-number <number>', 'Issue number')
-  .option('-o, --org <org_name>', 'Organisation name')
-  .option('-r, --repo <repo_name>', 'Repository name')
+  .requiredOption('-c, --command <command name>', 'Command to execute', core.getInput('command'))
+  .option('-t, --token <token>', 'Personal Access Token or GITHUB_TOKEN', core.getInput('token'))
+  .option('-i, --issue-number <number>', 'Issue number', core.getInput('issue-number'))
+  .option('-o, --org <org_name>', 'Organisation name', core.getInput('org'))
+  .option('-r, --repo <repo_name>', 'Repository name', core.getInput('repo'))
   .parse();
   
 /**
@@ -25,8 +34,8 @@ program
     const options = program.opts();
     const invoker = new Invoker(options);
     const result = await invoker.executeCommand(options);
-    console.log(`✨ OUTPUT: ${result}`);
+    core.setOutput(result);
   } catch (Error) {
-    console.error(`⚠️  ERROR: ${Error.message}`);
+    core.setFailed(` ⚠️  ${Error.message}`);
   }
 })()
